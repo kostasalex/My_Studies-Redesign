3a71a0cde2fc15c82e63eef41d52589c7a6e6b97
Kostas Alex <sdi1700003@di.uoa.gr>
2023-12-14 00:42:28 +0200

    fix: change paths

--- client/src/components/index.js
+++ client/src/components/index.js
@@ -1,5 +1,5 @@
-export { default as Header } from '/src/components/common/Header/Header';
-export { default as Footer } from '/src/components/common/Footer/Footer';
-export { default as Guest } from '/src/components/UserSpecific/guest/Guest';
-export { default as Student } from '/src/components/UserSpecific/student/Student';
-export { default as Teacher } from '/src/components/UserSpecific/teacher/Teacher';
+export { default as Header } from './common/header/Header';
+export { default as Footer } from './common/footer/Footer';
+export { default as Guest } from './UserSpecific/guest/Guest';
+export { default as Student } from './UserSpecific/student/Student';
+export { default as Teacher } from './UserSpecific/teacher/Teacher';
