9fec500d57e323392cbd35f7daab9a07a86a1322
Kostas Alex <sdi1700003@di.uoa.gr>
2023-12-15 00:11:09 +0200

    feat(login): add simple validation to the  login form

--- client/src/components/UserSpecific/guest/login/LoginForm.jsx
+++ client/src/components/UserSpecific/guest/login/LoginForm.jsx
@@ -1,19 +1,47 @@
-import styles from "./LoginForm.module.css";
+import { Form, Field } from 'react-final-form';
+import { loginValidator, validateWithZod } from './validation';
+import styles from './LoginForm.module.css';
 
 const LoginForm = () => {
+  const handleSubmit = (values) => {
+    console.log("Login submitted", values);
+  };
+
   return (
     <div className={styles["login-form"]}>
       <img src="/src/assets/logo-large.png" alt="Login" className={styles["login-image"]} />
-      <form>
       <h1>My - Studies</h1>
-        <div className={styles["input-group"]}>
-          <input type="text" id="username" name="username" placeholder="---username---" required />
-        </div>
-        <div className={styles["input-group"]}>
-          <input type="password" id="password" name="password" placeholder="---password---" required />
-        </div>
-        <button type="submit" className={styles["login-button"]}>Login</button>
-      </form>
+      <Form
+        onSubmit={handleSubmit}
+        validate={validateWithZod(loginValidator)}
+        render={({ handleSubmit, submitting }) => (
+          <form onSubmit={handleSubmit}>
+            <Field name="userName">
+              {({ input, meta }) => (
+                <div className={styles["input-group"]}>
+                  <label htmlFor="userName">Username</label>
+                  <input {...input} type="text" placeholder="Username" />
+                  {meta.error && meta.touched && <span>{meta.error}</span>}
+                </div>
+              )}
+            </Field>
+            <Field name="password">
+              {({ input, meta }) => (
+                <div className={styles["input-group"]}>
+                  <label htmlFor="password">Password</label>
+                  <input {...input} type="password" placeholder="Password" />
+                  {meta.error && meta.touched && <span>{meta.error}</span>}
+                </div>
+              )}
+            </Field>
+            <div className='d-flex justify-content-center'>
+              <button className= "btn btn-primary" type="submit" disabled={submitting}>
+                Login
+              </button>
+            </div>
+          </form>
+        )}
+      />
     </div>
   );
 };
