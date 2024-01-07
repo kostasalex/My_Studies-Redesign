0617c6220aef0aed49a2bc3219f22e2ca83897ae
Giannakius <giannakius10@gmail.com>
2024-01-07 17:19:56 +0200

    Header

--- client/src/components/common/header/Header.jsx
+++ client/src/components/common/header/Header.jsx
@@ -1,4 +1,5 @@
 import { useNavigate } from "react-router-dom";
+import { Dropdown } from "react-bootstrap";
 import styles from "./Header.module.css";
 
 export default function Header() {
@@ -19,19 +20,21 @@ export default function Header() {
   };
 
   const handleLoginStudent = () => {
-    // Προσθέστε τον κώδικα για τη σύνδεση του φοιτητή
     console.log("Σύνδεση Φοιτητή");
   };
 
-  const handleImageClick = () => {
-    // Προσθέστε τον κώδικα για την εικόνα που λειτουργεί ως κουμπί
-    console.log("Κλικ στην εικόνα");
+  const handleImageClick = (selectedOption) => {
+    console.log(`Επιλογή: ${selectedOption}`);
   };
 
   return (
     <div className={`${styles.header} p-2`}>
       <div className={`${styles.headerContainer} m-2`}>
-        <img src="LOGO_UOA COL2.jpg" alt="UOA Logo" className={styles.logo} />
+        <img
+          src="https://res.cloudinary.com/drijmbypg/image/upload/c_pad,b_auto:predominant,fl_preserve_transparency/v1702593334/logo-large_uaskki.jpg?_s=public-apps"
+          alt="UOA Logo"
+          className={`${styles.logoleft}`}
+        />
         <button
           type="button"
           className={`${styles.button} btn btn-primary m-2`}
@@ -53,20 +56,46 @@ export default function Header() {
         >
           Βοήθεια
         </button>
+
         <button
           type="button"
-          className={`${styles.button} btn btn-primary m-2`}
-          onClick={handleLoginStudent}
+          className={`${styles.loginbutton} btn btn-primary`}
+          onClick={() => handleUserChange("teacher")}
         >
-          Σύνδεση Φοιτητή
+          Είσοδος Φοιτητών
         </button>
+
         <button
           type="button"
-          className={`${styles.roundButton} m-2`}
-          onClick={handleImageClick}
+          className={`${styles.loginbutton} btn btn-primary m-2`}
+          onClick={() => handleUserChange("teacher")}
         >
-          <img src="/greek-flag.jpg" alt="Greek Flag" />
+          Είσοδος Καθηγητών
         </button>
+
+        <Dropdown>
+          <Dropdown.Toggle
+            variant="primary"
+            id="dropdown-basic"
+            className={`${styles.button} ${styles.logoright}`}
+          >
+            <img
+              src="https://www.pngitem.com/pimgs/m/23-231113_clip-art-greek-flag-png-greek-flag-circle.png"
+              alt="Greek"
+              style={{ width: "30px", height: "auto" }}
+            />
+          </Dropdown.Toggle>
+
+          <Dropdown.Menu style={{ width: "10px", textAlign: "center" }}>
+            <Dropdown.Item onClick={() => handleImageClick("Option 1")}>
+              <img
+                src="https://static.vecteezy.com/system/resources/previews/025/863/131/original/united-kingdom-flag-circle-flag-of-uk-in-round-circle-png.png"
+                alt="English"
+                style={{ width: "30px", height: "auto" }}
+              />
+            </Dropdown.Item>
+          </Dropdown.Menu>
+        </Dropdown>
       </div>
     </div>
   );
