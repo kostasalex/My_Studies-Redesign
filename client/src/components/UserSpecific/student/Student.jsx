39a35d2836a012ce724bc5adc40edfee2633ca50
Giannakius <giannakius10@gmail.com>
2024-01-10 18:44:05 +0200

    feat(profile)

--- client/src/components/UserSpecific/student/Student.jsx
+++ client/src/components/UserSpecific/student/Student.jsx
@@ -1,20 +1,20 @@
-import { Route, Routes, useNavigate } from 'react-router-dom';
-import { Navigate } from 'react-router-dom';
-import React from 'react';
-import Dashboard from '@/components/common/dashboard/Dashboard';
-import { studentDashboardButtons } from '@/locales/gr';
-import Home from "./home/Home"
+import { Route, Routes, useNavigate } from "react-router-dom";
+import { Navigate } from "react-router-dom";
+import React from "react";
+import Dashboard from "@/components/common/dashboard/Dashboard";
+import { studentDashboardButtons } from "@/locales/gr";
+import Home from "./home/Home";
 
-import styles from './Student.module.css';
-import Registration from './registration/Registration';
-import Grades from './grades/Grades';
-import Certificates from './certificates/Certificates';
-import NewRegistration from './registration/newRegistration/NewRegistration';
+import styles from "./Student.module.css";
+import Registration from "./registration/Registration";
+import Grades from "./grades/Grades";
+import Certificates from "./certificates/Certificates";
+import NewRegistration from "./registration/newRegistration/NewRegistration";
+import Profile from "./Profile/Profile";
 
 export default function Student() {
-
   const navigate = useNavigate();
-  const [selected, setSelected] = React.useState('dashboard'); // default selected state
+  const [selected, setSelected] = React.useState("dashboard"); // default selected state
 
   const handleButtonClick = (path) => {
     setSelected(path);
@@ -24,19 +24,21 @@ export default function Student() {
   //! Fix when redirect to /dashboard , previous selected state (registration) doesnt change to dashboard
   //! Fix when click Registration from new registration doesnt redirected back to the Registration
   return (
-    <div style={{ display: 'flex' }}>
+    <div style={{ display: "flex" }}>
       <Dashboard>
-        {['dashboard', 'registration', 'grades', 'certificates', 'profile'].map((path, index) => (
-          <button
-            key={path}
-            className={selected === path ? styles.selectedButton : ''}
-            onClick={() => handleButtonClick(path)}
-          >
-            {studentDashboardButtons[index]}
-          </button>
-        ))}
+        {["dashboard", "registration", "grades", "certificates", "profile"].map(
+          (path, index) => (
+            <button
+              key={path}
+              className={selected === path ? styles.selectedButton : ""}
+              onClick={() => handleButtonClick(path)}
+            >
+              {studentDashboardButtons[index]}
+            </button>
+          )
+        )}
       </Dashboard>
-      <div style={{ padding: '20px', flex: 1 }}>
+      <div style={{ padding: "20px", flex: 1 }}>
         <Routes>
           <Route path="/" element={<Navigate replace to="dashboard" />} />
           <Route path="dashboard" element={<Home />} />
@@ -45,7 +47,7 @@ export default function Student() {
           </Route>
           <Route path="grades" element={<Grades />} />
           <Route path="certificates" element={<Certificates />} />
-          <Route path="profile" element={<div>Προφίλ</div>} />
+          <Route path="profile" element={<Profile />} />
         </Routes>
       </div>
     </div>
