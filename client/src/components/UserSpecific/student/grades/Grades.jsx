4f866c3364b934c356a21374491bfd42970a7971
Giannakius <giannakius10@gmail.com>
2024-01-10 15:54:00 +0200

    feat(homepage)

--- client/src/components/UserSpecific/student/grades/Grades.jsx
+++ client/src/components/UserSpecific/student/grades/Grades.jsx
@@ -1,36 +1,47 @@
-import Pagination from "@/components/common/Pagination"
-import Table from "./table/Table"
-
+import Pagination from "@/components/common/Pagination";
+import Table from "./table/Table";
 
 const Grades = () => {
-    return (
-        <div>
-            <div>
-                <div className="btn-group m-5">
-                    <button type="button" className="btn btn-secondary dropdown-toggle" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-                        Περίοδος
-                    </button>
-                </div>
-                <div className="btn-group">
-                    <button type="button" className="btn btn-secondary dropdown-toggle" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-                        Έτος
-                    </button>
-                </div>
-            </div>
-            <div className="d-flex justify-content-between mb-3">
-                <h4>Χειμερινό 2013</h4>
-                <button className="btn btn-primary">Κατέβασε εδώ</button>
-            </div>
-            <Table />
-            <div className="d-flex justify-content-between mb-3 mt-5">
-                <h4>Εαρινό 2013</h4>
-            </div>
-                <Table />
-            <div className="d-flex justify-content-center mt-5">
-                <Pagination/>
-            </div>
+  return (
+    <div>
+      <div>
+        <div className="btn-group m-5">
+          <button
+            type="button"
+            className="btn btn-primary dropdown-toggle"
+            data-toggle="dropdown"
+            aria-haspopup="true"
+            aria-expanded="false"
+          >
+            Περίοδος
+          </button>
         </div>
-    )
-}
+        <div className="btn-group">
+          <button
+            type="button"
+            className="btn btn-primary dropdown-toggle"
+            data-toggle="dropdown"
+            aria-haspopup="true"
+            aria-expanded="false"
+          >
+            Έτος
+          </button>
+        </div>
+      </div>
+      <div className="d-flex justify-content-between mb-3">
+        <h4>Χειμερινό 2013</h4>
+        <button className="btn btn-primary">Κατέβασε εδώ</button>
+      </div>
+      <Table />
+      <div className="d-flex justify-content-between mb-3 mt-5">
+        <h4>Εαρινό 2013</h4>
+      </div>
+      <Table />
+      <div className="d-flex justify-content-center mt-5">
+        <Pagination />
+      </div>
+    </div>
+  );
+};
 
-export default Grades
+export default Grades;
--- client/src/components/UserSpecific/student/home/NotificationsSection.jsx
+++ client/src/components/UserSpecific/student/home/NotificationsSection.jsx
@@ -1,11 +1,28 @@
-import styles from './NotificationsSection.module.css'
+import styles from "./NotificationsSection.module.css";
 
 const NotificationsSection = () => {
   return (
-    <div className={styles["notifications-section"]} >
-        <h3>Ειδοποιήσεις:</h3>
+    <div className={styles["notifications-section"]}>
+      <h3>Ειδοποιήσεις:</h3>
+      <ul>
+        <li>
+          <h5 style={{ color: "blue", textDecoration: "underline" }}>
+            Εγκρίθηκε το Πιστοποιητικό Φοιτητικής Ιδιότητας.
+          </h5>
+        </li>
+        <li>
+          <h5 style={{ color: "blue", textDecoration: "underline" }}>
+            Νέος Βαθμός στην Γραμμική Άλγεβρα!
+          </h5>
+        </li>
+        <li>
+          <h5 style={{ color: "blue", textDecoration: "underline" }}>
+            Η Περίοδος Δήλωσης Μαθημάτων Ξεκίνησε.
+          </h5>
+        </li>
+      </ul>
     </div>
-  )
-}
+  );
+};
 
-export default NotificationsSection
+export default NotificationsSection;
