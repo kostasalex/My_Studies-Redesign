0acd2e7d0e575e8914870b32573774e3da47bad9
Kostas Alex <sdi1700003@di.uoa.gr>
2024-01-13 18:35:55 +0200

    fix(footer): better position + margin from the top

--- client/src/components/common/footer/Footer.jsx
+++ client/src/components/common/footer/Footer.jsx
@@ -1,22 +1,21 @@
 import styles from './Footer.module.css';
-import {footerData} from '@/locales/gr';
+import { footerData } from '@/locales/gr';
 
 
 const Footer = () => (
-    <>
-        <div className={styles.footer}>
+    <div className={styles.footer}>
+        <div>
             {footerData.map((card) => (
                 <div key={card.id}>
                     <a href={card.link} target={card.link.startsWith('http') ? '_blank' : '_self'}
-                       rel="noopener noreferrer">
+                        rel="noopener noreferrer">
                         {card.title}
                     </a>
                 </div>
             ))}
-
         </div>
         <div className={styles.footerText}>My Studies UoA © 2024</div>
-    </>
+    </div>
 );
 
 export default Footer;
