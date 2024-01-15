985d274cdd182dd4403b2af216235c0e472aac17
Giannakius <giannakius10@gmail.com>
2024-01-15 14:29:36 +0200

    .

--- client/src/components/UserSpecific/student/home/GradesSection.jsx
+++ client/src/components/UserSpecific/student/home/GradesSection.jsx
@@ -1,5 +1,5 @@
 import styles from "./GradesSection.module.css";
-import gradesimage from "./temp.png";
+
 import DoughnutChart from "./DoughnutChart";
 
 const GradesSection = () => {
@@ -7,14 +7,16 @@ const GradesSection = () => {
     <div className={styles["grades-section"]}>
       <h2>Βαθμολόγιο: </h2>
       <DoughnutChart passed={10} failed={5} noGrade={3} />
-      <div style={{ display: "flex", justifyContent: "space-between" }}>
-        <h5 style={{ textDecoration: "underline" }}>Πιστοτικές Μονάδες :</h5>
-        <h5 style={{ textDecoration: "underline" }}>Δηλωμένα Μαθήματα :</h5>
-      </div>
+      <div className={styles["ects-container"]}>
+        <div className={styles["ects"]}>
+          <h5 style={{ textDecoration: "underline" }}>Πιστοτικές Μονάδες :</h5>
+          <h5>170/240 ECTS</h5>
+        </div>
 
-      <div style={{ display: "flex", justifyContent: "space-between" }}>
-        <h5>170/240 ECTS</h5>
-        <h5>19 Μαθήματα</h5>
+        <div className={styles["ects"]}>
+          <h5 style={{ textDecoration: "underline" }}>Δηλωμένα Μαθήματα :</h5>
+          <h5>19 Μαθήματα</h5>
+        </div>
       </div>
     </div>
   );
