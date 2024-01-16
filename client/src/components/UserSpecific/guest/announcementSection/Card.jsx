f20b13ef550c06ad498aa96723338104ec6968eb
Giannakius <giannakius10@gmail.com>
2024-01-16 06:52:30 +0200

    lastestnews

--- client/src/components/UserSpecific/guest/announcementSection/Card.jsx
+++ client/src/components/UserSpecific/guest/announcementSection/Card.jsx
@@ -1,12 +1,24 @@
-import styles from './Card.module.css';
+import styles from "./Card.module.css";
 
-const Card = ({ image, title }) => {
-    return (
-        <div className={styles.card}>
-            <img src={image} alt={title}/>
-            <h2 >{title}</h2>
-        </div>
-    );
+const Card = ({ image, title, link }) => {
+  const handleCardClick = () => {
+    if (link) {
+      window.open(link, "_blank");
+    }
+  };
+
+  return (
+    <div className={styles.card} onClick={handleCardClick}>
+      <img src={image} alt={title} />
+      {link ? (
+        <a href={link} target="_blank" rel="noopener noreferrer">
+          <h2>{title}</h2>
+        </a>
+      ) : (
+        <h2>{title}</h2>
+      )}
+    </div>
+  );
 };
 
 export default Card;
