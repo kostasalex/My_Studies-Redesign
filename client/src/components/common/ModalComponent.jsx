9ed2751fdbdc51a484cca0054c3d0364fdfd16b1
Kostas Alex <sdi1700003@di.uoa.gr>
2024-01-17 21:05:01 +0200

    feat: implement a common modal component  (dialog box)

---
+++ client/src/components/common/ModalComponent.jsx
@@ -0,0 +1,25 @@
+import { useState, useCallback } from 'react';
+import { Modal } from 'react-bootstrap';
+
+
+export const useModalControl = () => {
+    const [isOpen, setIsOpen] = useState(false);
+
+    const openModal = useCallback(() => setIsOpen(true), []);
+    const closeModal = useCallback(() => setIsOpen(false), []);
+
+    return { isOpen, openModal, closeModal };
+};
+
+const ModalComponent = ({ isOpen, closeModal, title, children }) => {
+    return (
+        <Modal show={isOpen} onHide={closeModal}>
+            <Modal.Header closeButton>
+                <Modal.Title>{title}</Modal.Title>
+            </Modal.Header>
+            <Modal.Body>{children}</Modal.Body>
+        </Modal>
+    );
+};
+
+export default ModalComponent;
