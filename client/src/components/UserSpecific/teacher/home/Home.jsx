bc15fabc81e8bf2a9d6b141fd8205bc1f233d63a
Giannakius <giannakius10@gmail.com>
2024-01-27 19:58:13 +0200

    Σχεδιαστικα

--- client/src/components/UserSpecific/teacher/home/Home.jsx
+++ client/src/components/UserSpecific/teacher/home/Home.jsx
@@ -1,21 +1,18 @@
-
-
-import CurrentCourses from "./CurrentCourses"
-import styles from "./Home.module.css"
-import OpenGrades from "./OpenGrades"
-import RecentGrades from "./RecentGrades"
+import CurrentCourses from "./CurrentCourses";
+import styles from "./Home.module.css";
+import OpenGrades from "./OpenGrades";
+import RecentGrades from "./RecentGrades";
 
 const Home = () => {
-    return (
-        <div className="d-flex flex-column">
-            <div className={styles["section-wrapper"]}>
-                <CurrentCourses />
-                <RecentGrades />
-                <OpenGrades />
-            </div>
+  return (
+    <div className="d-flex flex-column">
+      <div className={styles["section-wrapper"]}>
+        <CurrentCourses />
+        <RecentGrades />
+        <OpenGrades />
+      </div>
+    </div>
+  );
+};
 
-        </div >
-    )
-}
-
-export default Home
+export default Home;
