e0d74c2cb29dcc79da5706b11c734a865d71063e
Giannakius <giannakius10@gmail.com>
2024-01-28 15:00:50 +0200

    Breadcrumbs

--- client/src/components/common/Breadcrumbs.jsx
+++ client/src/components/common/Breadcrumbs.jsx
@@ -1,28 +1,34 @@
-import { useLocation } from 'react-router-dom';
-import { Breadcrumb } from 'react-bootstrap';
+import { useLocation } from "react-router-dom";
+import { Breadcrumb } from "react-bootstrap";
+import { FaHome, FaUser, FaFile, FaFolder } from "react-icons/fa"; // Εισαγωγή εικονιδίων από FontAwesome
 
 const Breadcrumbs = () => {
-    const location = useLocation();
-    const pathnames = location.pathname.split('/').filter(x => x);
+  const location = useLocation();
+  const pathnames = location.pathname.split("/").filter((x) => x);
 
-    return (
-        <Breadcrumb>
-            <Breadcrumb.Item href="/">Home</Breadcrumb.Item>
-            {pathnames.map((name, index) => {
-                const routeTo = `/${pathnames.slice(0, index + 1).join('/')}`;
-                const isLast = index === pathnames.length - 1;
-                return isLast ? (
-                    <Breadcrumb.Item active key={name}>
-                        {name}
-                    </Breadcrumb.Item>
-                ) : (
-                    <Breadcrumb.Item key={name} href={routeTo}>
-                        {name}
-                    </Breadcrumb.Item>
-                );
-            })}
-        </Breadcrumb>
-    );
+  return (
+    <Breadcrumb>
+      <Breadcrumb.Item href="/">
+        <FaHome /> Home
+      </Breadcrumb.Item>
+      {pathnames.map((name, index) => {
+        const routeTo = `/${pathnames.slice(0, index + 1).join("/")}`;
+        const isLast = index === pathnames.length - 1;
+
+        const icon = name === "user" ? <FaUser /> : <FaFolder />;
+
+        return isLast ? (
+          <Breadcrumb.Item active key={name}>
+            {icon} {name}
+          </Breadcrumb.Item>
+        ) : (
+          <Breadcrumb.Item key={name} href={routeTo}>
+            {icon} {name}
+          </Breadcrumb.Item>
+        );
+      })}
+    </Breadcrumb>
+  );
 };
 
 export default Breadcrumbs;
