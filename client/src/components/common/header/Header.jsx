6a1dcee363dd4bbf7796fcfead3fdda211d00508
Kostas Alex <sdi1700003@di.uoa.gr>
2024-01-28 16:34:05 +0200

    styles(header): fix language position

--- client/src/components/common/header/Header.jsx
+++ client/src/components/common/header/Header.jsx
@@ -80,18 +80,20 @@ const Header = () => {
             id="basic-navbar-nav"
             className={styles.navbarCollapse}
           >
-            <Nav className="me-auto">
+            <Nav className="me-auto ">
               <Nav.Link href="/">{headerTexts.home}</Nav.Link>
               <Nav.Link href="/faq">{headerTexts.faq}</Nav.Link>
               <Nav.Link href="/contact">{headerTexts.contact}</Nav.Link>
+
+            </Nav>
+
+            <div className={styles.languagelogin}>
               <div className={styles.languageSwitcher}>
                 <FaGlobe onClick={toggleLanguage} />
                 <span onClick={toggleLanguage}>
                   {language === "en" ? "English" : "Ελληνικά"}
                 </span>
               </div>
-            </Nav>
-            <div className={styles.languagelogin}>
               <div>
                 {isUserLoggedIn ? (
                   <Dropdown align="mid">
